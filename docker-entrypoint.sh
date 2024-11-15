c1defc336ddf6e7e2fab6305c45777ac48cc6ae6
kokoye2007 <kokoye2007@gmail.com>
2024-11-15 21:50:34 +1100

    update docker-entrypoint.sh time to checking log

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,3 +19,35 @@ while [ ! -f /pkg/main/sys-process.tini.core/bin/tini-static ]; do
 done
 
 exec /pkg/main/sys-process.tini.core/bin/tini-static "$@"
+
+#!/usr/azusa/busybox sh
+set -e
+
+# Start apkg daemon in the background and log output
+/usr/azusa/apkg >/var/log/apkg.log 2>&1 </dev/null &
+
+wait_interval=0.5  # seconds
+elapsed_time=0
+
+# Use `until` to wait for the specific log message indicating readiness
+until /usr/azusa/busybox grep -q "ctrl: control socket ready" /var/log/apkg.log; do
+    #echo "Waiting for apkg to be ready..."
+    sleep "$wait_interval"
+    clear
+    elapsed_time=$(echo "$elapsed_time + $wait_interval" | bc)
+    echo "$elapsed_time"
+    tail -n 5 /var/log/apkg.log
+done
+
+echo -e '
+      ___                         ____  _____
+     /   |____  __  ___________ _/ __ \/ ___/
+    / /| /_  / / / / / ___/ __ `/ / / /\__ \
+   / ___ |/ /_/ /_/ (__  ) /_/ / /_/ /___/ /
+  /_/  |_/___/\__,_/____/\__,_/\____//____/
+        https://github.com/AzusaOS
+
+'
+
+# Execute tini-static with passed arguments once the file is available
+exec /pkg/main/sys-process.tini.core/bin/tini-static "$@"
